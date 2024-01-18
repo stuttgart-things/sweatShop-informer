5c212a0316a470572d9b131d745492cea23f1866
Patrick Hermann <patrick.hermann@sva.de>
2024-01-18 15:21:13 +0000

    updated stageTime-informer 24.0118.1521 for tag version v0.1.39

--- internal/converter_test.go
+++ internal/converter_test.go
@@ -4,8 +4,15 @@ Copyright © 2023 PATRICK HERMANN patrick.hermann@sva.de
 
 package internal
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestVerifyJobCompletionStatus(t *testing.T) {
 
+	prStatus := "{{0 [{Succeeded False  {2024-01-18 12:28:56 +0000 UTC} Failed Tasks Completed: 2 (Failed: 1, Cancelled 0)"
+	regexPattern := `Succeeded\s(\w+)`
+	jobStatus := verifyJobCompletionStatus(prStatus, regexPattern)
+	fmt.Println(jobStatus)
 }
