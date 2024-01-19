4e62e7c9338d238a253bf00fb07a0d4b37db5149
Patrick Hermann <patrick.hermann@sva.de>
2024-01-19 08:50:27 +0000

    updated stageTime-informer 24.0119.0850 for tag version v0.1.42

--- internal/producer.go
+++ internal/producer.go
@@ -8,6 +8,9 @@ import (
 	"fmt"
 	"os"
 
+	goredis "github.com/redis/go-redis/v9"
+	sthingsCli "github.com/stuttgart-things/sthingsCli"
+
 	"github.com/redis/go-redis/v9"
 
 	"context"
@@ -16,6 +19,7 @@ import (
 var (
 	redisUrl      = os.Getenv("REDIS_SERVER") + ":" + os.Getenv("REDIS_PORT")
 	redisPassword = os.Getenv("REDIS_PASSWORD")
+	redisClient   = goredis.NewClient(&goredis.Options{Addr: redisUrl, Password: redisPassword, DB: 0})
 )
 
 func produceStatus(key, value string) {
@@ -42,15 +46,12 @@ func produceStatus(key, value string) {
 
 func checkStageStatus(pipelineRunLabels map[string]string) {
 
-	fmt.Println(pipelineRunLabels)
-	// ctx := context.TODO()
-	// rdb := redis.NewClient(&redis.Options{
-	// 	Addr:     redisUrl,
-	// 	Password: redisPassword, // no password set
-	// 	DB:       0,             // use default DB
-	// })
+	stagePipelineRuns := sthingsCli.GetValuesFromRedisSet(redisClient, pipelineRunLabels["stagetime/stage"])
+	fmt.Println("ALL PIPELEINRUNS OF THIS STAGE: ", stagePipelineRuns)
 
 }
 
 // pipelineRuns := sthingsCli.GetValuesFromRedisSet(redisClient, stageStatusFromRedis.StageID)
 // log.Info("ALL PIPELEINRUNS OF THIS STAGE: ", pipelineRuns)
+
+// │ map[stagetime/author:patrick-hermann-sva stagetime/commit:3c5ac44c6fec00989c7e27b36630a82cdfd26e3b0 stagetime/repo:stuttgart-things stagetime/stage:0 tekton.dev/pipeline:st-0-simu │
